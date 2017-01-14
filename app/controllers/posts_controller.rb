a6a5dc228b3e47b26e686f3cdb0c8ce8c9710e33
wangxiaolong <shengdaomoluo@gmail.com>
2017-01-14 22:36:10 +0800

    post edit/delete  create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,30 +21,30 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:group_id])
-      @post = Post.find([:id])
-  end
-
-  def update
-    @group = Group.find(params[:group_id])
+  @group = Group.find(params[:group_id])
     @post = Post.find(params[:id])
-    if @post.update(post_params)
-      redirect_to account_posts_path, notice: 'Post Update Success!'
-    else
-      render :edit
-  end
+end
 
-  def destroy
-    @group = Group.find(params[:group_id])
-    @post = Post.find([:id])
-    @post.destroy
-    redirect_to account_posts_path, alert: 'Post deleted!'
-  end
-  
+def update
+  @group = Group.find(params[:group_id])
+  @post = Post.find(params[:id])
+  if @post.update(post_params)
+    redirect_to account_posts_path, notice: 'Post Update Success!'
+  else
+    render :edit
+end
+
+def destroy
+  @group = Group.find(params[:group_id])
+  @post = Post.find(params[:id])
+  @post.destroy
+  redirect_to account_posts_path, alert: 'Post deleted!'
+end
   private
 
   def post_params
     params.require(:post).permit(:content)
   end
 
+end
 end
